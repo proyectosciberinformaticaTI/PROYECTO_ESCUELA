4074a38c989a5bb792d06ac2e3ca9a8203288085
Joel Anthony Arauzo Masgo <projectinformatic1196@gmail.com>
2020-08-31 00:36:14 -0500

    Primera Versión

---
+++ src/main/java/com/principal/proyecto_escuela/ProyectoEscuelaApplication.java
@@ -0,0 +1,13 @@
+package com.principal.proyecto_escuela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEscuelaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoEscuelaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/principal/proyecto_escuela/ProyectoEscuelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.principal.proyecto_escuela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEscuelaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
